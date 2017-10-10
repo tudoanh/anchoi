3bad7a11993db366e70d6f2f74b8f7e32f2c8c88
tu <tu0703@gmail.com>
2017-10-10 07:59:19 +0700

    update

--- anchoi/anchoi/settings.py
+++ anchoi/anchoi/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'anchoi_drf',
-        'USER': 'tu',
-        'PASSWORD': 'Tu0703$$',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
